4175405281a4cb06e852f73ba2bdb24a6b4c9e99
Mohammad Hussein Tavakoli Bina <mhtb32@gmail.com>
2020-06-12 23:45:05 +0430

    changes version to 0.1.0

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='tl-env',
-    version='0.0.1',
+    version='0.1.0',
     author='Mohammad Hussein Tavakoli Bina',
     author_email='mhtb32@gmail.com',
     description='A package based on highway-env for temporal logic based rewards',
